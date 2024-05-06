da23a744ad8478a10014d268c4b48ee4fac1dd6e
Mint Rosetta <mint.rosetta2001@gmail.com>
2024-05-06 20:42:32 +0700

    code - single responsibility principle

---
+++ sources/design_principles - SRP - hands on begin/src/main/java/com/company/singleresp/UserController.java
@@ -0,0 +1,32 @@
+package com.company.singleresp;
+
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+//Handles incoming JSON requests that work on User resource/entity
+public class UserController {
+	//Store used by controller
+    private UserPersistenceService userPersistenceService = new UserPersistenceService();
+    
+    //Create a new user
+    public String createUser(String userJson) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        
+        User user = mapper.readValue(userJson, User.class);
+        
+        UserValidator userValidator = new UserValidator();
+        boolean valid = userValidator.validateUser(user);
+
+        if(!valid) {
+            return "ERROR";
+        }
+
+        this.userPersistenceService.saveUser(user);
+        
+        return "SUCCESS";
+    } 
+
+}
