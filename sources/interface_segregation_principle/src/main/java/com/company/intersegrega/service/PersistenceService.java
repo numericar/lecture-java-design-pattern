c893560a7a3c44885c488240fd5a5f9cad28a07b
Mint Rosetta <mint.rosetta2001@gmail.com>
2024-05-09 17:42:13 +0700

    code: interface segregation principle

--- sources/interface_segregation_principle/src/main/java/com/company/intersegrega/service/PersistenceService.java
+++ sources/interface_segregation_principle/src/main/java/com/company/intersegrega/service/PersistenceService.java
@@ -1,7 +1,5 @@
 package com.company.intersegrega.service;
 
-import java.util.List;
-
 import com.company.intersegrega.entity.Entity;
 
 //common interface to be implemented by all persistence services. 
@@ -12,6 +10,5 @@ public interface PersistenceService<T extends Entity> {
 	public void delete(T entity);
 	
 	public T findById(Long id);
-	
-	public List<T> findByName(String name);
+
 }
