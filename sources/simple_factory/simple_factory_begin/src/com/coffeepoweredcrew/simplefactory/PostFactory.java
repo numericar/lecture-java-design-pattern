6c3df982bf265ae4dc9a46ba065857124d5ec72d
Mint Rosetta <mint.rosetta2001@gmail.com>
2024-05-16 21:25:39 +0700

    code: simple factory

--- sources/simple_factory/simple_factory_begin/src/com/coffeepoweredcrew/simplefactory/PostFactory.java
+++ sources/simple_factory/simple_factory_begin/src/com/coffeepoweredcrew/simplefactory/PostFactory.java
@@ -6,5 +6,16 @@ package com.coffeepoweredcrew.simplefactory;
  *
  */
 public class PostFactory {
-
+    public static Post createPost(String type) {
+        switch (type) {
+            case "blog":
+                return new BlogPost();
+            case "news":
+                return new NewsPost();
+            case "product":
+                return new ProductPost();
+            default:
+                throw new IllegalArgumentException("Post type is unknow");
+        }
+    }
 }
